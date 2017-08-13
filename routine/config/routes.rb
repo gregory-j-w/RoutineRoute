7287fbfbb3592ec51f8625ae6017ca1326ace46e
Greg Wilwerding <greg.wilwerding@gmail.com>
2017-08-13 16:12:19 -0500

    paths and links

--- routine/config/routes.rb
+++ routine/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'application#hello'
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get  'static_pages/addroute'
-
+  root 'static_pages#home'
+  get  '/about',   to: 'static_pages#about'
+  get  '/addroute', to: 'static_pages#addroute'
+  get  '/signup',  to: 'users#new'
 end
