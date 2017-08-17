ff1cbfb827a442db152bff7086c3469fa576f9c8
Greg Wilwerding <greg.wilwerding@gmail.com>
2017-08-16 21:07:41 -0500

    l routes in-progress

--- routine/app/controllers/ltrains_controller.rb
+++ routine/app/controllers/ltrains_controller.rb
@@ -1,5 +1,27 @@
 class LtrainsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
+  def ltrainsroute
+  end
+
+  def ltrainsdestination
+    @rt = params[:rt]
+  end
+
+  def ltrainsdestination
+  end
+
+  def ltrainsave
+  end
+
+
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
 
 end
