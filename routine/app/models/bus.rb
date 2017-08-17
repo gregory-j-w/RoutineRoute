3ebf16e85482918c8466fe871f3fb64c8152ef4f
Greg Wilwerding <greg.wilwerding@gmail.com>
2017-08-17 17:08:03 -0500

    fixes

--- routine/app/models/bus.rb
+++ routine/app/models/bus.rb
@@ -2,7 +2,6 @@ class Bus < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :name, presence: true, length: { maximum: 60 }
   validates :rt, presence: true
   validates :rtnm, presence: true
   validates :rtdir, presence: true
